3b86ff481ea8ea6b0bd6e486b7f6824ff11f1342
Anders Strandberg <jae2meter@gmail.com>
2015-05-07 14:53:40 +0200

    7

--- 6evalu.go
+++ 6evalu.go
@@ -5,30 +5,37 @@ import (
 	"time"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-
-	args := os.Args
-	fileName := os.Args[1]
+	
+	var fileName string;
+	fileName = os.Args[1];
+	//var maxDiffTime int;
+	maxDiffTimeS := os.Args[2];
+	maxDiffTime, err := strconv.ParseInt (maxDiffTimeS, 10, 64);
 	var nowTime, fileTime int64;
 
-	mt.Println (fileName):
+	// fmt.Println (fileName);
 	//  Get current time in seconds since epoch 
 	now := time.Now();
 	nowTime = now.Unix();
 	
-	fmt.Printf("Time in seconds is %d\n", nowTime);
+	// fmt.Printf("Time in seconds is %d\n", nowTime);
 
 	//  Get file modificaton time in seconds since epoch 
-	info, err := os.Stat("evalu.go")
+	info, err := os.Stat(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fileTime = info.ModTime().Unix();
-//	fmt.Println(info.ModTime().Unix());
-	fmt.Println(fileTime)
-	fmt.Println (nowTime - fileTime);
+	// fmt.Println(info.ModTime().Unix());
+	//fmt.Println(nowTime)
+	//fmt.Println (fileTime - fileTime);
+	if (nowTime-fileTime) > maxDiffTime {
+		fmt.Printf("Diff is big %d\n", (nowTime-fileTime));
+	}
 	os.Exit (1)
 }
